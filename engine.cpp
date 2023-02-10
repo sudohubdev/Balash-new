57fcde20f43d3675739d65425ace7f742a74658e
Denis(mac) <denistesla2005@gmail.com>
2023-02-10 18:55:31 +0200

    fix retina

--- engine.cpp
+++ engine.cpp
@@ -18,7 +18,11 @@ Renderer::Renderer()
     glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_FALSE);
+    #endif
+    
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
     // Open a window and create its OpenGL context
     win = glfwCreateWindow(resx, resy, "balash", NULL, NULL);
